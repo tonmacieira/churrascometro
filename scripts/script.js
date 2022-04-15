0bc3ec17d5ba6089d65a360056828340697ec099
tonmacieira <100812318+tonmacieira@users.noreply.github.com>
2022-04-15 13:27:41 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ var pessoa = prompt('Nome de usuário ?')
 
 var usuario = document.getElementById('usuario')
 
-// usuario.innerHTML = `Olá, ${pessoa}, obrigado por utilizar nossos serviços`
+
 
 let inputadultos = document.getElementById('adultos')
 let inputcriancas = document.getElementById('criancas')
@@ -23,36 +23,38 @@ function calcular() {
     let duracao = inputduracao.value
 
     let qtdcarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas)
-    let qtdcerveja = cervejaPP(duracao) * adultos 
+    let qtdcerveja = cervejaPP(duracao) * adultos
     let qtdbebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas)
 
     usuario.innerHTML = `Olá, ${pessoa}, obrigado por utilizar nossos serviços <br> <p>Para o seu churrasco irá precisar de: </p>`
     resultadocarne.innerHTML = `${qtdcarne / 1000} kg de carnes`
-    resultadocerveja.innerHTML = `${qtdcerveja / 1000} Litros de cervejas`
-    resultadorefri.innerHTML = `${qtdbebidas / 1000} Litros de bebidas (aguá, refrigerantes)`
+    resultadocerveja.innerHTML = `${Math.ceil(qtdcerveja / 335)} Latas de cervejas`
+    resultadorefri.innerHTML = `${qtdbebidas / 2000} Litros de bebidas (aguá, refrigerantes)`
 
 }
 
-function carnePP(duracao){
-    if(duracao >= 6) {
+// foi criado 3 funções para lidar com, carne, cerveja, e bebidas
+
+function carnePP(duracao) { // se a duração for maior que 6 horas, aumentar para 650 gramas de carne
+    if (duracao >= 6) {
         return 650
-    }else{
-        return 400;
+    } else {
+        return 400; // ou se a duração for menos que 6 horas, será de 400 gramas
     }
 }
 
-function cervejaPP(duracao){
-    if(duracao >= 6) {
+function cervejaPP(duracao) {   // se a duração for maior que 6 horas, aumentar para 2000
+    if (duracao >= 6) {
         return 2000
-    }else{
-        return 1200;
+    } else {
+        return 1200;  // se a duração for menor que 6 horas, manter 12000
     }
 }
 
-function bebidasPP(duracao){
-    if(duracao >= 6) {
+function bebidasPP(duracao) {  // se a duração for maior que 6 horas, aumentar para 650 gramas de carne
+    if (duracao >= 6) {
         return 1500
-    }else{
-        return 1000;
+    } else {
+        return 1000;  // se a duração for menor que 6 horas, manter 1000
     }
 }
